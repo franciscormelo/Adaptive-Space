23c6858ad37fb94a74ee1c5dfc7c5f9c5c862bd6
Francisco Melo <francisco.melo@hotmail.com>
2020-03-06 16:24:38 +0000

    Multiple_groups

--- space_modelingv2.py
+++ space_modelingv2.py
@@ -39,6 +39,7 @@ def draw_person_top(x, y, angle, ax):
     top_x = HUMAN_X / 2
     plot_ellipse(semimaj=top_x, semimin=top_y, phi=angle, x_cent=x, y_cent=y, ax=ax)
 
+
 def draw_personalspace(x, y, angle, ax, sx, sy, plot_kwargs, idx):
     """ """
     draw_arrow(x, y, angle)  # orientation arrow angle in radians
@@ -51,6 +52,7 @@ def draw_personalspace(x, y, angle, ax, sx, sy, plot_kwargs, idx):
 
     return patches.Ellipse((x,y),sx*2,sy*2,math.degrees(angle)) # Multiply sx and sy by 2 to get the diameter
 
+
 def approachingfiltering(personal_space, approaching_filter, idx):
     # Approaching Area filtering - remove points tha are inside the personal space of a person
     if idx == 1:
@@ -62,143 +64,174 @@ def approachingfiltering(personal_space, approaching_filter, idx):
     return approaching_filter
 
 
+def group_radius(persons, group_pose):
+    """ """
+    sum_radius = 0
+    for i in range(len(persons)):
+        # average of the distance between the group members and the center of the group, o-space radius
+        sum_radius = sum_radius + euclidean_distance(persons[i][0], persons[i][1], group_pose[0], group_pose[1])
+    return sum_radius / len(persons)
+
 class SpaceModeling:
 
     def __init__(self, fh):
         """ """
+
+        # Lists Initialization
+
         # split the file into lines
         lines = fh.read().splitlines()
 
         # removes blank lines
         file = list(filter(None, lines))
+        n = len(file) # Number of groups in the file
 
-        for string in file:
-            group = tuple(string.split(","))
-            self.group_nb = len(group) # Numbers of members in a group
+        # Lists intialization
+        self.group_nb = []
+        self.persons = [[] for i in range(n)]
+        self.group_pose = []
+        self.group_radius = []
+        for num, string in enumerate(file):
+            print(num)
+            data = string.split("--")
+            group = tuple(data[0].split(","))
+            self.group_nb.append(len(group)) # Numbers of members in a group
 
-            self.persons = []
+
             for person in group:
                 pose = tuple(person.split(" "))
-                self.persons.append(tuple([float(pose[0][1:]), float(pose[1]), float(pose[2][:-1])]))
+                self.persons[num].append(tuple([float(pose[0][1:]), float(pose[1]), float(pose[2][:-1])]))
 
-            self.persons = tuple(self.persons)
-            self.group_pose = (0, 0)  # o space center point
+            self.persons[num] = tuple(self.persons[num])
 
-            # compute group radius given the center of the o-space
-            self.group_radius = self.group_radius()
 
-    def group_radius(self):
-        """ """
-        sum_radius = 0
-        for i in range(len(self.persons)):
-            # average of the distance between the group members and the center of the group, o-space radius
-            sum_radius = sum_radius + euclidean_distance(self.persons[i][0], self.persons[i][1], self.group_pose[0], self.group_pose[1])
-        return sum_radius / len(self.persons)
+            # Saves the group center i.e. o space center
+            group_center = data[1].split(",")
+            self.group_pose.append(tuple([float(group_center[0][1:]), float(group_center[1][:-1])]))  # o space center point
+
+            # computes group radius given the center of the o-space
+            radius = group_radius(self.persons[num], self.group_pose[num])
+            self.group_radius.append(radius)
 
 
     def solve(self):
         """ """
         f, ax = plt.subplots(1)
 
-        # O Space Modeling
-        ax.plot(self.group_pose[0], self.group_pose[1], 'rx', markersize=8)
-        plot_kwargs = {'color': 'r', 'linestyle': '-', 'linewidth': 1}
-        plot_ellipse(semimaj=self.group_radius - HUMAN_X/2, semimin=self.group_radius - HUMAN_X/2, x_cent=self.group_pose[0],
-                     y_cent=self.group_pose[1], ax=ax, plot_kwargs=plot_kwargs)
-
-        # p Space Modeling
-        plot_ellipse(semimaj=self.group_radius + HUMAN_X/2, semimin=self.group_radius + HUMAN_X/2, x_cent=self.group_pose[0],
-                     y_cent=self.group_pose[1], ax=ax, plot_kwargs=plot_kwargs)
-
-        # approaching circle area
-        plot_kwargs = {'color': 'c', 'linestyle': ':', 'linewidth': 2}
-        plot_ellipse(semimaj=self.group_radius, semimin=self.group_radius, x_cent=self.group_pose[0],
-                            y_cent=self.group_pose[1], ax=ax, plot_kwargs=plot_kwargs)
-        approaching_area = plot_ellipse(semimaj=self.group_radius, semimin=self.group_radius, x_cent=self.group_pose[0],
-                            y_cent=self.group_pose[1],data_out = True)
-
-        # compute mean distance between group members
-        d_sum = 0
-        for i in range(len(self.persons) - 1):
-            # average of the distance between group members
-            d_sum = d_sum + euclidean_distance(self.persons[i][0], self.persons[i][1], self.persons[i + 1][0],
-                                              self.persons[i + 1][1])
-        d_mean = d_sum / len(self.persons)
-###############################################################################
-        if self.group_nb == 2:
-
-            #tentar fazer de maneira automatica mas por enquanto fazer por tipo de grupo
-            if self.persons[0][2] == self.persons[1][2]: # side-by-side
 
 
-                sy = euclidean_distance(self.persons[0][0], self.persons[0][1], self.persons[1][0],
-                                                  self.persons[1][1])/2
-                sx = sy * 1.5
+
+        for k in range(len(self.group_nb)):
+            print("Modeling Group " + str(k+1) + " ...")
+
+            group_radius = self.group_radius[k]
+            group_pose = self.group_pose[k]
+            persons = self.persons[k]
+            group_nb = self.group_nb[k]
+
+            # O Space Modeling
+            ax.plot(group_pose[0], group_pose[1], 'rx', markersize=8)
+            plot_kwargs = {'color': 'r', 'linestyle': '-', 'linewidth': 1}
+            plot_ellipse(semimaj=group_radius - HUMAN_X/2, semimin=group_radius - HUMAN_X/2, x_cent=group_pose[0],
+                         y_cent=group_pose[1], ax=ax, plot_kwargs=plot_kwargs)
+
+            # p Space Modeling
+            plot_ellipse(semimaj=group_radius + HUMAN_X/2, semimin=group_radius + HUMAN_X/2, x_cent=group_pose[0],
+                         y_cent=group_pose[1], ax=ax, plot_kwargs=plot_kwargs)
+
+            # approaching circle area
+            plot_kwargs = {'color': 'c', 'linestyle': ':', 'linewidth': 2}
+            plot_ellipse(semimaj=group_radius, semimin=group_radius, x_cent=group_pose[0],
+                                y_cent=group_pose[1], ax=ax, plot_kwargs=plot_kwargs)
+            approaching_area = plot_ellipse(semimaj=group_radius, semimin=group_radius, x_cent=group_pose[0],
+                                y_cent=group_pose[1],data_out = True)
+
+            # compute mean distance between group members
+            d_sum = 0
+################################# Meter em função ##############################
+            for i in range(len(persons) - 1):
+                # average of the distance between group members
+                d_sum = d_sum + euclidean_distance(persons[i][0], persons[i][1], persons[i + 1][0],
+                                                  persons[i + 1][1])
+            d_mean = d_sum / len(persons)
+    ############################################################################
+            if group_nb == 2:
+
+                #tentar fazer de maneira automatica mas por enquanto fazer por tipo de grupo
+                if persons[0][2] == persons[1][2]: # side-by-side
 
 
-                if sy > PSPACEY: # if the persons are too far away from each other the personal space should be limited
+                    sy = euclidean_distance(persons[0][0], persons[0][1], persons[1][0],
+                                                      persons[1][1])/2
+                    sx = sy * 1.5
+
+
+                    if sy > PSPACEY: # if the persons are too far away from each other the personal space should be limited
+                        sx = PSPACEX
+                        sy = PSPACEY
                     sx = PSPACEX
                     sy = PSPACEY
-                sx = PSPACEX
-                sy = PSPACEY
 
-            elif abs(round(self.persons[0][2] + math.pi,2))  == abs(round(self.persons[1][2],2)): # vis-a-vis
+                elif abs(round(persons[0][2] + math.pi,2))  == abs(round(persons[1][2],2)): # vis-a-vis
 
-                sx = euclidean_distance(self.persons[0][0], self.persons[0][1], self.persons[1][0],
-                                                  self.persons[1][1])/2
-                sy = sx/1.5
+                    sx = euclidean_distance(persons[0][0], persons[0][1], persons[1][0],
+                                                      persons[1][1])/2
+                    sy = sx/1.5
 
+                    if sy < HUMAN_Y/2: #the personal space should be at least the size of the individual
+                        sy = HUMAN_Y/2
+
+                    if sx > PSPACEX : # if the persons are too far away from each other the personal space should be limited
+                        sx = PSPACEX
+                        sy = PSPACEY
+                else:
+                        sx = d_mean # radius in x
+                        sy = sx / 1.5 # radius in y
+
+                        if sx > PSPACEX or sy > PSPACEY:
+                            sx = PSPACEX
+                            sy = PSPACEY
+
+            # variar a maneira como e calculado tendo em conta o tipo de grupo
+            else: # The typical arragement  of a group of more than 2 persons is tipically circular
+
+            # Scaling factors for personal space
+                sx = d_mean # radius in x
+                sy = sx / 1.5 # radius in y
                 if sy < HUMAN_Y/2: #the personal space should be at least the size of the individual
                     sy = HUMAN_Y/2
 
-                if sx > PSPACEX : # if the persons are too far away from each other the personal space should be limited
-                    sx = PSPACEX
-                    sy = PSPACEY
-            else:
-                    sx = d_mean # radius in x
-                    sy = sx / 1.5 # radius in y
+            if sx > PSPACEX or sy > PSPACEY:
 
-                    if sx > PSPACEX or sy > PSPACEY:
-                        sx = PSPACEX
-                        sy = PSPACEY
-
-        # variar a maneira como e calculado tendo em conta o tipo de grupo
-        else: # The typical arragement  of a group of more than 2 persons is tipically circular
-
-        # Scaling factors for personal space
-            sx = d_mean # radius in x
-            sy = sx / 1.5 # radius in y
-            if sy < HUMAN_Y/2: #the personal space should be at least the size of the individual
-                sy = HUMAN_Y/2
-
-        if sx > PSPACEX or sy > PSPACEY:
-            print(entrei)
-            sx = PSPACEX
-            sy = PSPACEY
+                sx = PSPACEX
+                sy = PSPACEY
 
 
 
 
-###############################################################################
-        idx = 1
-        plot_kwargs = {'color': 'g', 'linestyle': '-', 'linewidth': 0.8}
+    ###############################################################################
 
-        approaching_filter = approaching_area
+            plot_kwargs = {'color': 'g', 'linestyle': '-', 'linewidth': 0.8}
 
-        for person in self.persons:
+            approaching_filter = approaching_area
 
-            personal_space = draw_personalspace(person[0], person[1], person[2], ax, sx, sy, plot_kwargs, idx)
+            for idx, person in enumerate(persons, start=1):
 
-            # Approaching Area filtering - remove points tha are inside the personal space of a person
-            approaching_filter = approachingfiltering(personal_space, approaching_filter, idx)
-            idx = idx + 1
+                personal_space = draw_personalspace(person[0], person[1], person[2], ax, sx, sy, plot_kwargs, idx)
 
-        # possible approaching positions
-        approaching_x = [j[0] for j in approaching_filter]
-        approaching_y = [k[1] for k in approaching_filter]
+                # Approaching Area filtering - remove points tha are inside the personal space of a person
+                approaching_filter = approachingfiltering(personal_space, approaching_filter, idx)
 
 
-        ax.plot(approaching_x, approaching_y, 'c.',  markersize = 5)
+            # possible approaching positions
+            approaching_x = [j[0] for j in approaching_filter]
+            approaching_y = [k[1] for k in approaching_filter]
+
+
+            ax.plot(approaching_x, approaching_y, 'c.',  markersize = 5)
+
+
+
         plt.xlabel('x [cm]')
         plt.ylabel('y [cm]')
         plt.savefig('destination_path.eps', format='eps')
