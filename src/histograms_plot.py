e300220a4d1590445dd77eeb4413f2c0e1936da0
Francisco Melo <francisco.melo@hotmail.com>
2020-05-18 18:46:28 +0100

    Plots histograms as percentage

--- src/histograms_plot.py
+++ src/histograms_plot.py
@@ -6,12 +6,13 @@
     Date created: X/XX/XXXX
     Date last modified: X/XX/XXXX
     Python Version: 3.7
+    https://malithjayaweera.com/2018/09/add-matplotlib-percentage-ticks-histogram/
 '''
 import sys
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from matplotlib.ticker import PercentFormatter
-
+import matplotlib.ticker as ticker
 import numpy as np
 
 
@@ -19,6 +20,7 @@ class Histogram:
     """Creates Histograms for personal space parameters."""
 
     def __init__(self, fh):
+        """ """
         # Lists Initialization
 
         # split the file into lines
@@ -45,23 +47,42 @@ class Histogram:
             self.group_info['param_x'].append(float(params_xy[0]))
             self.group_info['param_y'].append(float(params_xy[1]))
 
-    def to_percent(y, position):
-        # Ignore the passed in position. This has the effect of scaling the default
-        # tick locations.
-    s = str(100 * y)
+    def histogram_1d(self, axs, n_bins, x, y):
+        """ Plots 1d histogram"""
+    # We can set the number of bins with the `bins` kwarg
+        N, bins, patches = axs[0].hist([x, y], bins='auto', label=['x', 'y'],
+                                       alpha=0.7, rwidth=0.85)
+        axs[0].set_xlabel(r'Personal Space $x,y$ $(cm)$')
+        axs[0].set_ylabel(r'Frequency')
+        axs[0].grid(axis='y', alpha=0.75)
+        axs[0].legend(loc='upper right')
+        axis_y = axs[0].twinx()
+        axis_y.hist([x, y], bins='auto', label=['x', 'y'],
+                    alpha=0.7, rwidth=0.85)
+        axis_y.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=len(x)))
+        axis_y.set_ylabel(r'Percentage', rotation=270, labelpad=20)
 
-    # The percent symbol needs escaping in latex
-    if matplotlib.rcParams['text.usetex'] is True:
-        return s + r'$\%$'
-    else:
-        return s + '%'
+    def histogram_2d(self, axs, n_bins, x, y):
+        """ """
+
+        cset1 = axs[1].hist2d(x, y, bins=n_bins)
+        axs[1].set_xlabel(r'Personal Space $x$ $(cm)$')
+        axs[1].set_ylabel(r'Personal Space $y$ $(cm)$')
+        axs[1].grid(axis='y', alpha=0.75)
+
+        cbar = plt.colorbar(cset1[3], ax=axs[1])
+        cbar.ax.set_ylabel('Frequency', rotation=270, labelpad=10)
+        #plt.savefig('figures/histograms_full.eps', format='eps')
+        cbar_per = plt.colorbar(cset1[3], ax=axs[1])
+        cbar_per.ax.yaxis.set_major_formatter(
+            ticker.PercentFormatter(xmax=len(x)))
+        cbar_per.ax.set_ylabel('Percentage', rotation=270, labelpad=10)
 
     def solve(self, number_elements):
         """ Plots Histogram of x and y parameters of personal space"""
         x = self.group_info['param_x']
         y = self.group_info['param_y']
-        #fig, ax = plt.subplots(tight_layout=True)
-        # hist =
+
         plt.rc('text', usetex=True)
         plt.rc('font', family='serif')
         plt.style.use('seaborn-deep')
@@ -70,23 +91,11 @@ class Histogram:
 
         fig, axs = plt.subplots(1, 2, sharey=False, tight_layout=True)
 
-        # We can set the number of bins with the `bins` kwarg
-        N, bins, patches = axs[0].hist([x, y], bins='auto', label=['x', 'y'],
-                                       alpha=0.7, rwidth=0.85)
-        axs[0].set_xlabel(r'Personal Space $x,y$ $(cm)$')
-        axs[0].set_ylabel(r'Frequency')
+        self.histogram_1d(axs, 20, x, y)
         axs[0].set_title(r'Persons Space Histogram $x,y$ axis - All Groups')
-        axs[0].grid(axis='y', alpha=0.75)
-        axs[0].legend(loc='upper right')
-
-        cset1 = axs[1].hist2d(x, y)
-        axs[1].set_xlabel(r'Personal Space $x$ $(cm)$')
-        axs[1].set_ylabel(r'Personal Space $y$ $(cm)$')
+        self.histogram_2d(axs, 20, x, y)
         axs[1].set_title(r'Personal Space 2D Histogram - All Groups')
-        axs[1].grid(axis='y', alpha=0.75)
-        plt.colorbar(cset1[3], ax=axs[1])
-        #plt.savefig('figures/histograms_full.eps', format='eps')
-        print("Number of groups: " + str(len(self.group_info['group_nb'])))
+        print("Number of groups: " + str(len(x)))
 
         fig2, axs2 = plt.subplots(1, 2, sharey=False, tight_layout=True)
         g_x = []
@@ -98,24 +107,14 @@ class Histogram:
                 g_x.append(self.group_info['param_x'][i])
                 g_y.append(self.group_info['param_y'][i])
 
-        axs2[0].hist([g_x, g_y], bins='auto', label=['x', 'y'],
-                     alpha=0.7, rwidth=0.85)
-        axs2[0].set_xlabel(r'Personal Space $x,y$ $(cm)$')
-        axs2[0].set_ylabel(r'Frequency')
+        self.histogram_1d(axs2, 20, g_x, g_y)
         axs2[0].set_title(
             r'Persons Space Histogram $x,y$ axis - %d Members' % number_elements)
-        axs2[0].grid(axis='y', alpha=0.75)
-        axs2[0].legend(loc='upper right')
-
-        cset2 = axs2[1].hist2d(g_x, g_y)
-        axs2[1].set_xlabel(r'Personal Space $x$ $(cm)$')
-        axs2[1].set_ylabel(r'Personal Space $y$ $(cm)$')
+        self.histogram_2d(axs2, 20, g_x, g_y)
         axs2[1].set_title(
             r'Personal Space 2D Histogram - %d Members' % number_elements)
-        axs2[1].grid(axis='y', alpha=0.75)
-        plt.colorbar(cset2[3], ax=axs2[1])
-        print("Number of groups of " + str(number_elements) +
-              " elements: " + str(len(g_x)))
+
+        print("Number of groups: " + str(len(g_x)))
 
         #plt.savefig('figures/histograms_2.eps', format='eps')
         plt.show(block=False)
