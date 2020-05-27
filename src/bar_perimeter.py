b82be47e46ebff3ce02fe8c35b6eb090c431dc94
Francisco Melo <francisco.melo@hotmail.com>
2020-05-27 22:12:09 +0100

    Fixed bar plots

--- src/bar_perimeter.py
+++ src/bar_perimeter.py
@@ -54,12 +54,37 @@ class Bar:
                         xytext=(0, 3),  # 3 points vertical offset
                         textcoords="offset points",
                         ha='center', va='bottom')
+    def sum_perimeter(self):
+        """ Return the sum of perimeter of all groups and grouped by number o members"""
+        total_perim_fixed = 0 
+        total_perim_adaptive = 0
+        
+        # Maximum number of members in a group
+        max_members = max(self.group_info['group_nb'])
+        gperim_adap = [0] * max_members
+        gperim_fixed = [0] * max_members
+        total_perim_fixed =  sum(self.group_info['fixed_perim'])
+        total_perim_adaptive = sum(self.group_info['adaptive_perim'])
+        
+        
+        for group_nb in self.group_info['group_nb']:
+            gperim_adap[group_nb - 1] += self.group_info['adaptive_perim'][group_nb - 1 ]
+            gperim_fixed[group_nb - 1] += self.group_info['fixed_perim'][group_nb - 1]
+            
+
+            
+        return gperim_adap, gperim_fixed, total_perim_fixed, total_perim_adaptive
+        
 
     def solve(self):
         """ """
         adaptive_perimeter = self.group_info['adaptive_perim']
         fixed_perimeter = self.group_info['fixed_perim']
+        
+        
+        gperim_adap, gperim_fixed, total_perim_fixed, total_perim_adaptive = self.sum_perimeter()
 
+        
         plt.rc('text', usetex=True)
         plt.rc('font', family='serif')
         plt.style.use('seaborn-deep')
@@ -67,37 +92,36 @@ class Bar:
         fig, ax = plt.subplots(1, 2, tight_layout=True)
 
         labels = []
-        for i, perimeter in enumerate(adaptive_perimeter):
-            labels.append("G" + str((i + 1)))
+        nb_labels = range(max(self.group_info['group_nb']))
+        for i in nb_labels:
+            labels.append( str((i + 1))  )
 
         x = np.arange(len(labels))  # the label locations
         width = 0.35  # the width of the bars
 
-        rects1 = ax[0].bar(x - width / 2, adaptive_perimeter,
+        rects1 = ax[0].bar(x - width / 2, gperim_adap,
                            width, label='Adaptive Parameters')
-        rects2 = ax[0].bar(x + width / 2, fixed_perimeter,
+        rects2 = ax[0].bar(x + width / 2, gperim_fixed,
                            width, label='Fixed Parameters')
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax[0].set_ylabel(r'Perimeter $(cm)$')
-        ax[0].set_xlabel(r'Groups')
+        ax[0].set_xlabel(r'Number of members')
         ax[0].set_title(r'Approaching Perimeter Comparison')
         ax[0].set_xticks(x)
         ax[0].set_xticklabels(labels)
         ax[0].legend()
 
-        self.autolabel(rects1, ax[0])
-        self.autolabel(rects2, ax[0])
+        # self.autolabel(rects1, ax[0])
+        # self.autolabel(rects2, ax[0])
 
-        sum_adaptive = sum(adaptive_perimeter)
-        sum_fixed = sum(fixed_perimeter)
         labels_sum = [""]
         x = np.arange(len(labels_sum))  # the label locations
 
         rects1_sum = ax[1].bar(
-            x - width / 2, round(sum_adaptive, 0), width, label='Adaptive Parameters')
+            x - width / 2, round(total_perim_adaptive, 0), width, label='Adaptive Parameters')
         rects2_sum = ax[1].bar(
-            x + width / 2, round(sum_fixed, 0), width, label='Fixed Parameters')
+            x + width / 2, round(total_perim_fixed, 0), width, label='Fixed Parameters')
 
         # Add some text for labels, title and custom x-axis tick labels, etc.
         ax[1].set_ylabel(r'Sum Perimeter $(cm)$')
